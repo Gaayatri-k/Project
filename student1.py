80ec2061503a02b98ce7af99a67152df23b2493c
Gaayatri-k <24b01a1235@svecw.edu.in>
2025-09-25 22:17:13 +0530

    added students successfully

--- student1.py
+++ student1.py
@@ -21,3 +21,27 @@ def write_students(students):
 def ensure_reports_folder():
     if not os.path.exists(Reportfolder):
         os.makedirs(Reportfolder)
+def add_student():
+    students = read_students()
+    roll = input("Enter Roll No: ")
+    if any(s["Roll_No"] == roll for s in students):
+        print("Duplicate Roll No.")
+        return
+    name = input("Enter Name: ")
+    branch = input("Enter Branch: ")
+    year = input("Enter Year: ")
+    gender = input("Enter Gender: ")
+    age = input("Enter Age: ")
+    attendance = input("Enter Attendance %: ")
+    mid1 = input("Mid1 Marks: ")
+    mid2 = input("Mid2 Marks: ")
+    quiz = input("Quiz Marks: ")
+    final = input("Final Marks: ")
+    new_student = {
+        "Roll_No": roll, "Name": name, "Branch": branch, "Year": year,
+        "Gender": gender, "Age": age, "Attendance": attendance,
+        "Mid1": mid1, "Mid2": mid2, "Quiz": quiz, "Final": final
+    }
+    students.append(new_student)
+    write_students(students)
+    print("Student added successfully.")
