fc1e5edaa1425f43c1da72743a39970e1424ace8
Gaayatri-k <24b01a1235@svecw.edu.in>
2025-09-25 21:52:56 +0530

    Readed file successfully

--- student1.py
+++ student1.py
@@ -5,3 +5,11 @@ import statistics
 Filename="studentsmanagement.csv"
 Deletedfile="students_deleted.csv"
 Reportfolder="reports"
+def read_students():
+    if not os.path.exists():
+        with open(Filename, "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Roll_No","Name","Branch","Year","Gender","Age",
+                             "Attendance","Mid1","Mid2","Quiz","Final"])
+    with open(Filename, newline="") as f:
+        return list(csv.DictReader(f))
